1d6c1127b80b05adfe78e823745d11ad9d7b1751
giridhar <giribala2001@gmail.com>
2023-08-07 23:07:57 +0530

    completeUI

--- src/Components/Vector/Vector.js
+++ src/Components/Vector/Vector.js
@@ -3,14 +3,13 @@ import "../Vector/Vector.css"
 
 export default function Vector() {
     return (
-        <div class="framer-x9m1eb" 
-        style={{opacity: 1, transform: "translateX(0px) translateY(-351.774px) scale(1) rotate(0deg) rotateX(0deg) rotateY(0deg) translateZ(0px)"}}>
-            <div class="framer-8mavu0"></div>
-            <div class="framer-16x9014"></div>
-            <div class="framer-1lvsclt"></div>
-            <div class="framer-1x4en10"></div>
-            <div class="framer-bpo1nj"></div>
-            <div class="framer-15daoq4"></div>
+        <div class="framer" >
+            <div class="shape framer1"></div>
+            <div class="shape framer2"></div>
+            <div class="shape framer3"></div>
+            <div class="shape framer4"></div>
+            <div class="shape framer5"></div>
+            <div class="shape framer6"></div>
         </div>
     )
 }
