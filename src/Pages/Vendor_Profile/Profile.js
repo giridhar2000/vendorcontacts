bfe3c830d9e5aeaae6828fc5e3c1447c55f5f98f
Basu-Dev-Ghosh <basu1735@gmail.com>
2023-09-12 20:40:20 +0530

    Profile update done

--- src/Pages/Vendor_Profile/Profile.js
+++ src/Pages/Vendor_Profile/Profile.js
@@ -3,15 +3,17 @@ import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import bg1 from "../../Assets/img/img1.jpg";
 import logo from "../../Assets/img/gensler_logo.png";
-import { AiOutlineArrowRight } from "react-icons/ai";
+import { AiOutlineArrowRight, AiOutlineUser, AiFillEdit } from "react-icons/ai";
 import "./Profile.css";
 import PdfCard from "../../Components/PdfCard/PdfCard";
 import supabase from "../../utils/supabase.config";
 import { getUser } from "../../utils/profile_helper";
 import { useQuery } from "react-query";
-import { Skeleton } from "antd";
+import { Empty, Skeleton } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
+  const navigate=useNavigate();
   const { data: profile, isLoading } = useQuery("profile", getUser);
 
   if (isLoading) {
@@ -24,27 +26,12 @@ const Profile = () => {
             <Skeleton.Image active={isLoading} />
           </div>
         </div>
-        <div className="profile-body">
-          <div className="profile-info">
-            <div className="profile-name">
-            <Skeleton
-            active={isLoading}
-            paragraph={{
-              rows: 3,
-            }}
-          />
-            </div>
-            <div className="profile-about">
-              <p>
-              <Skeleton
-              active={isLoading}
-              paragraph={{
-                rows: 2,
-              }}
-            />
-              </p>
-            </div>
-          </div>
+        <div
+          className="profile-body"
+          style={{ width: "100%", justifyContent: "center", margin: "40px 0" }}
+        >
+          <div className="profile-info"></div>
+
           <div className="profile-box center">
             <Skeleton
               active={isLoading}
@@ -69,12 +56,6 @@ const Profile = () => {
               }}
             />
             <hr />
-            <div className="pdf-cards">
-              <PdfCard />
-              <PdfCard />
-              <PdfCard />
-              <PdfCard />
-            </div>
           </div>
         </div>
 
@@ -87,58 +68,65 @@ const Profile = () => {
       <Header />
       <div className="cover-pic">
         <img src={bg1} alt="bg" />
-        <div className="profile-pic">
-          <img src={logo} alt="profile" />
-        </div>
+        {profile.profile_pic ? (
+          <div className="profile-pic">
+            <img src={profile?.profile_pic} alt="profile" />
+          </div>
+        ) : (
+          <div className="profile-pic no-pic">
+            <AiOutlineUser />
+          </div>
+        )}
       </div>
       <div className="profile-body">
         <div className="profile-info">
           <div className="profile-name">
-            <p>Gensler</p>
-            <p>Los angels</p>
+            <p>{profile?.display_name}</p>
+            <p>{profile?.location}</p>
             <p>
               Visit Rep's profile{" "}
               <button>
                 <AiOutlineArrowRight />
               </button>
             </p>
+            <p onClick={()=>navigate('/edit')}>
+              Edit profile{" "}
+              <button onClick={()=>navigate('/edit')}>
+                <AiFillEdit />
+              </button>
+            </p>
           </div>
           <div className="profile-about">
-            <p>
-              Gensler is a global architecture, design, and planning firm with
-              50 locations across Asia, Europe, Australia, the Middle East, and
-              the Americas. Founded in 1965, the firm serves more than 3,500
-              active clients. Aside from continuously being recognized on
-              top-ranking reports, their team receives many industry accolades
-              for stellar design work each year.
-            </p>
+            {profile?.bio ? (
+              <p>{profile?.bio}</p>
+            ) : (
+              <p>
+                <Empty description={"Update your Bio"} />
+              </p>
+            )}
           </div>
         </div>
         <div className="profile-box">
           <div className="line"></div>
-          <p>
-            Do not follow where the path may lead. Go instead where there is no
-            path and leave a trail
+          <p >
+          {
+            profile?.quote ?profile?.quote :"**Update your quote**"
+          }
+      
           </p>
         </div>
         <div className="profile-desc">
-          <h1>We Use Design to Create a Better World</h1>
-          <p>
-            Gensler is a global architecture, design, and planning firm with 53
-            locations and 7,000 professionals networked across the Americas,
-            Europe, Greater China, Asia Pacific, and the Middle East. Founded in
-            1965, the firm works globally with more than 4,000 clients across
-            more than 29 practice areas spanning the work, lifestyle, community,
-            and health sectors.
-            <br /> <br /> We are guided by our mission to create a better world
-            through the power of design and believe the source of our strength
-            is our people. By leveraging our diversity of ideas, our research
-            and innovation, our shared values, and our One-Firm Firm culture, we
-            are moving forward together into a new era defined by design. By
-            empowering our people and working seamlessly as an interconnected
-            firm, we’re able to make the greatest impact for our communities as
-            we continue to tackle the world’s challenges.
-          </p>
+          <h1>{
+          profile?.bio ? profile?.bio.substring(0,30):""
+          }</h1>
+          {profile?.bio ? (
+            <p>{profile?.bio}</p>
+          ) : (
+            <p>
+              <Empty description={"Update your Bio"} />
+            </p>
+          )}
+          
         </div>
         <div className="profile-downloads">
           <p>Downloads</p>
--- src/utils/profile_helper.js
+++ src/utils/profile_helper.js
@@ -5,19 +5,39 @@ export async function getUser() {
   let user = data?.user;
   let { data: profile, error } = await supabase
     .from("profiles")
-    .select('*')
+    .select("*")
     .eq("id", user?.id)
     .single();
   if (error) return error;
   return profile;
 }
 export async function getUserById(id) {
-  
   let { data: profile, error } = await supabase
     .from("profiles")
-    .select('*')
+    .select("*")
     .eq("id", id)
     .single();
   if (error) return error;
   return profile;
+}
+
+export async function updateUserProfile(
+  url,
+  name,
+  email,
+  location,
+  quote,
+  bio
+) {
+  let { data:user } = await supabase.auth.getUser();
+  let id = user?.user?.id;
+  
+  const { data, error } = await supabase
+    .from("profiles")
+    .update({ profile_pic: url,bio,location,quote,email,display_name:name })
+    .eq("id", id)
+   if(error){
+    return false;
+   }
+   return true;
 }
