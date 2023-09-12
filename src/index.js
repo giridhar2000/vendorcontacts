6310bdc038441f7c932fcf00d7a81345354b324e
GiridharHUB <scintillantmail@gmail.com>
2023-09-12 11:37:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <App />
-    <ToastContainer/>
-  </React.StrictMode>
+    <ToastContainer />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
