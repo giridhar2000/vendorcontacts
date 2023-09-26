907066e8f33c181af0af73807abf7e19e5cbac8d
GiridharHUB <scintillantmail@gmail.com>
2023-09-26 12:53:42 +0530

    Update PdfCard.js

--- src/Components/PdfCard/PdfCard.js
+++ src/Components/PdfCard/PdfCard.js
@@ -3,13 +3,13 @@ import "./PdfCard.css";
 import { BsDownload } from "react-icons/bs";
 import bg1 from "../../Assets/img/img1.jpg";
 import supabase from "../../utils/supabase.config";
+
 const PdfCard = ({ doc }) => {
   async function download() {
     const filePath=`public/${doc.name}`;
     const { data, error } = await supabase.storage
     .from("profile_docs")
     .download(filePath);
-    console.log(filePath)
     if(error){
       console.log(error)
       }
@@ -22,8 +22,7 @@ const PdfCard = ({ doc }) => {
       <div className="pdf-card-row1">
         <div className="pdf">PDF</div>
         <p>{doc?.name}</p>
-          <a target="_blank" href={`https://kzthdyjkhdwyqztvlvmp.supabase.co/storage/v1/object/public/profile_docs/public/${doc.name}`} download><BsDownload /></a>
-        
+          <a target="_blank" href={`https://kzthdyjkhdwyqztvlvmp.supabase.co/storage/v1/object/public/profile_docs/public/${doc?.name}`} download><BsDownload /></a>
       </div>
       <div className="pdf-card-image">
         <img src={bg1} alt="card img" />
