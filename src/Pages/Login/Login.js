f0f506afe872c5215756e002de8ac6dcf5a9087e
arshadiqbal06123 <arshadiqbal06123@gail.com>
2023-11-13 19:36:00 +0530

    login-again-update

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -598,9 +598,9 @@ export default function Login() {
                     </span>
                   </p>
                 </div>
-                {/* <div className="y-img"> */}
+                <div className="y-img">
                 <img src={loginbg} alt="login" className="loginimg singup-img" />
-                {/* </div> */}
+                </div>
               </>
             ) : (
               <div className="signup">
