77ad394ee8d693012415d834678b679b6dfce885
GiridharHUB <scintillantmail@gmail.com>
2023-11-28 17:28:06 +0530

    login changes

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -192,9 +192,12 @@ export default function Login() {
       <div className="loginContainer lg-page">
         {signup ? (
           <>
-            {/* <div className="loginText">
+            <div className="loginText">
               <h1>Welcome Back!</h1>
-              <span>Please provide your login details to access <br/> the content.</span>
+              <span>
+                Please provide your login details to access <br /> the
+                content.
+              </span>
             </div>
             <div className="Loginform">
               <div className="emailip">
@@ -308,155 +311,15 @@ export default function Login() {
               </button>
               <p className="p">
                 Don't have an account?{" "}
-                <span className="loginsignup" onClick={() => setSignUp(false)}>
+                <span
+                  className="loginsignup"
+                  onClick={() => setSignUp(false)}
+                >
                   <b>Sign Up</b>
                 </span>
               </p>
             </div>
-            
-            <img src={loginbg} alt="login" className="loginimg" id="loginimg" /> */}
-            {/* <div className="content"> */}
-              {/* <div className="wow loginContainer lg-page"> */}
 
-              <div className="loginText">
-                <h1>Welcome Back!</h1>
-                <span>
-                  Please provide your login details to access <br /> the
-                  content.
-                </span>
-              </div>
-              <div className="Loginform">
-                <div className="emailip">
-                  <input
-                    placeholder="Buisness Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <div className="passip">
-                  <input
-                    placeholder="Password"
-                    type={ip}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-
-                  {ip === "password" ? (
-                    <svg
-                      onClick={() => setIp("text")}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                      <g
-                        id="SVGRepo_tracerCarrier"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      ></g>
-                      <g id="SVGRepo_iconCarrier">
-                        {" "}
-                        <path
-                          d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12"
-                          stroke="#000000"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        ></path>{" "}
-                        <path
-                          d="M1 12C1 12 5 20 12 20C19 20 23 12 23 12"
-                          stroke="#000000"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        ></path>{" "}
-                        <circle
-                          cx="12"
-                          cy="12"
-                          r="3"
-                          stroke="#000000"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        ></circle>{" "}
-                      </g>
-                    </svg>
-                  ) : (
-                    <svg
-                      onClick={() => setIp("password")}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                      <g
-                        id="SVGRepo_tracerCarrier"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      ></g>
-                      <g id="SVGRepo_iconCarrier">
-                        {" "}
-                        <path
-                          d="M2 2L22 22"
-                          stroke="#000000"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        ></path>{" "}
-                        <path
-                          d="M6.71277 6.7226C3.66479 8.79527 2 12 2 12C2 12 5.63636 19 12 19C14.0503 19 15.8174 18.2734 17.2711 17.2884M11 5.05822C11.3254 5.02013 11.6588 5 12 5C18.3636 5 22 12 22 12C22 12 21.3082 13.3317 20 14.8335"
-                          stroke="#000000"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        ></path>{" "}
-                        <path
-                          d="M14 14.2362C13.4692 14.7112 12.7684 15.0001 12 15.0001C10.3431 15.0001 9 13.657 9 12.0001C9 11.1764 9.33193 10.4303 9.86932 9.88818"
-                          stroke="#000000"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        ></path>{" "}
-                      </g>
-                    </svg>
-                  )}
-                </div>
-                <p className="forgotpass">Forgot Password?</p>
-                <button
-                  className="loginbtn"
-                  disabled={loading}
-                  onClick={handleLogin}
-                >
-                  {loading ? (
-                    <>
-                      <Spin style={{ marginRight: ".5rem" }} /> Logging in...
-                    </>
-                  ) : (
-                    "Log in"
-                  )}
-                </button>
-                <p className="p">
-                  Don't have an account?{" "}
-                  <span
-                    className="loginsignup"
-                    onClick={() => setSignUp(false)}
-                  >
-                    <b>Sign Up</b>
-                  </span>
-                </p>
-              </div>
-              {/* </div> */}
-              <div className="imgi">
-                <img
-                  src={loginbg}
-                  alt="login"
-                  className="loginimg"
-                  id="loginimg"
-                />
-              </div>
-              
-            {/* </div> */}
-            
           </>
         ) : (
           <>
@@ -710,13 +573,13 @@ export default function Login() {
                     </span>
                   </p>
                 </div>
-                <div className="y-img">
+                {/* <div className="y-img">
                   <img
                     src={loginbg}
                     alt="login"
                     className="loginimg singup-img"
                   />
-                </div>
+                </div> */}
               </>
             ) : (
               <div className="signup">
@@ -773,6 +636,14 @@ export default function Login() {
             )}
           </>
         )}
+      </div>
+      <div className="imgi">
+        <img
+          src={loginbg}
+          alt="login"
+          className="loginimg"
+          id="loginimg"
+        />
       </div>
     </div>
   );
