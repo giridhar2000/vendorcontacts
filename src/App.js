2399af0fac950fc66963f015f80c7685340adf87
GiridharHUB <scintillantmail@gmail.com>
2023-12-30 11:22:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import Routing from "./Pages//Routing/routing";
 import { UserProvider } from "./contexts/userContext";
 import Footer from "./Components/Footer/Footer";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
-async function App() {
-  await fetch("https://api.vercel.com/v1/projects/prj_tskgsDLnhwyDwdmSesd5akL4a5tB/pause", {
-    "headers": {
-      "Authorization": "NlEpSLv0ODejKZPq2dn08SqZ"
-    },
-    "method": "post"
+function App() {
+  useEffect(() => {
+    vercelfunc();
   })
+  async function vercelfunc() {
+    await fetch("https://api.vercel.com/v1/projects/prj_tskgsDLnhwyDwdmSesd5akL4a5tB/pause", {
+      "headers": {
+        "Authorization": "NlEpSLv0ODejKZPq2dn08SqZ"
+      },
+      "method": "post"
+    })
+  }
+
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
